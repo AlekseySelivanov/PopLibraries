d5c2a3b387ee9be8711f1df2d860e3f370f4cb22
AlekseySelivanov <salex92@mail.ru>
2021-06-07 22:21:33 +0300

    HW3

--- app/src/main/java/com/example/poplibraries/mvp/model/GithubUsersRepo.kt
+++ app/src/main/java/com/example/poplibraries/mvp/model/GithubUsersRepo.kt
@@ -1,12 +1,15 @@
 package com.example.poplibraries.mvp.model
 
+import io.reactivex.rxjava3.core.Observable
+
 class GithubUsersRepo {
-    private val repositories = listOf(
+    private val repositoriesRX = Observable.just(
         GithubUser("login1"),
         GithubUser("login2"),
         GithubUser("login3"),
         GithubUser("login4"),
         GithubUser("login5")
     )
-    fun getUsers() : List<GithubUser> = repositories
+
+    fun getUsersRX() = repositoriesRX
 }
